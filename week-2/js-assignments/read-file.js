811ab0f56d30bb1904ba3c2bd9c65c7b290854e7
Nutileety <nutileety@gmail.com>
2024-08-25 22:05:43 +0530

    additional week2 js assignments

--- week-2/js-assignments/read-file.js
+++ week-2/js-assignments/read-file.js
@@ -1,7 +1,7 @@
 let fs = require('fs');
 
 let read_file = fs.readFileSync('a.txt','utf8');
-console.log(read_file);
+console.log(read_file); // output: This is read file of a.txt
 
 let read_file2 = fs.readFileSync('b.txt','utf8');
-console.log(read_file2);
+console.log(read_file2); // output: This is the text file of b.txt
