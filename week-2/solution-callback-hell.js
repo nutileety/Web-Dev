20e3d962abadfa1efe2c5d1b83390429de1088da
Nutileety <nutileety@gmail.com>
2024-09-13 20:20:09 +0530

    adding the callback-hell program in promisified way

--- week-2/solution-callback-hell.js
+++ week-2/solution-callback-hell.js
@@ -1,13 +1,13 @@
 // The solution in callback technique
-setTimeout(function () {
-    console.log("Hi");
-    setTimeout(function () {
-        console.log("Hello");
-        setTimeout(function (){
-            console.log("Holaa");
-        },5000);
-    }, 3000);
-}, 1000);
+// setTimeout(function () {
+//     console.log("Hi");
+//     setTimeout(function () {
+//         console.log("Hello");
+//         setTimeout(function (){
+//             console.log("Holaa");
+//         },5000);
+//     }, 3000);
+// }, 1000);
 
 /* Output:
     Hi          after  1 sec      
@@ -15,3 +15,25 @@ setTimeout(function () {
     Holaa       after  after 1+3+5 sec
     */
 
+// The solution in Promisified method.
+
+let setTimeoutPromisified = (ms) => {
+    return new Promise((resolve) => setTimeout(resolve,ms));
+}
+
+
+setTimeoutPromisified(1000).then(function () {
+    console.log("Hi");
+    setTimeoutPromisified(3000).then(function () {
+        console.log("Hello");
+        setTimeoutPromisified(5000).then(function () {
+            console.log("Holaa");
+        });
+    });
+});
+
+/* Output:
+    Hi          after  1 sec      
+    Hello       after  1+3 sec
+    Holaa       after  after 1+3+5 sec
+    */
---
+++ week-2/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+setTimeout(function () {
+//     console.log("Hi");
+//     setTimeout(function () {
+//         console.log("Hello");
+//         setTimeout(function (){
+//             console.log("Holaa");
+//         },5000);
+//     }, 3000);
+// }, 1000);
