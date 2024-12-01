81f78a74608120124fd4d1bd4595061e41aa7d5a
Nutileety <nutileety@gmail.com>
2024-12-01 20:03:15 +0530

    adding query for searching

--- node_practice/node_tut/server.js
+++ node_practice/node_tut/server.js
@@ -7,13 +7,18 @@ app.get('/', function(req, res) {
 });
 
 app.get('/idli', (req, res) => {
+    // Use the URL parameter 'qty' to specify how many idlis
+    var qty = req.params.qty || 2; // Get the quantity from the URL parameter
+    var sambar = req.query.sambar === false; // Query parameter for sambar
+    var chatni = req.query.chatni === false; // Query parameter for chutney
+
     var menu = {
         name:'rava idli',
-        qty: 2,
-        sambar: true,
-        chatni:false,
+        qty: qty,
+        sambar: sambar,
+        chatni: chatni,
     }
-    res.send(menu);
+    res.send(menu); // Send back the dynamic menu
 })
 app.listen(port, () => {
     console.log(`The server is using the port ${port}`);
