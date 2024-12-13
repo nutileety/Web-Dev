4642169cde0b9a87023943ef3530d9d350554d94
Nutileety <nutileety@gmail.com>
2024-12-13 10:03:26 +0530

    created the http-server log to store in log.txt file

--- node_practice/node_tut/http_server.js
+++ node_practice/node_tut/http_server.js
@@ -1,11 +1,22 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer((req, res) => {
     console.log("Done!");
     // console.log(req);
-    console.log(req.url); //prints the requested url
-    console.log(req.headers); //prints the header of the http request
-    res.end("Hi there, you are in http server");
+    // console.log(req.url); //prints the requested url
+    // console.log(req.headers); //prints the header of the http request
+
+    var log = `${Date.now()}: ${req.url} request received.\n `;
+    fs.appendFile('log.txt', log, (err) =>
+    {
+        if(err) {
+            res.end(err);
+        }
+        else {
+            res.end("Hi there, you are in http server");
+        }
+    })
 });
 
 server.listen(8000, () => {
