cb5e95ddbf1c4ff3fe4fb1c64e6ed063fd7579f2
Nutileety <nutileety@gmail.com>
2024-12-14 11:09:17 +0530

    adding the url module and get use of url module

--- node_practice/node_tut/http_server.js
+++ node_practice/node_tut/http_server.js
@@ -1,22 +1,38 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
 
 var server = http.createServer((req, res) => {
     console.log("Done!");
     // console.log(req);
     // console.log(req.url); //prints the requested url
     // console.log(req.headers); //prints the header of the http request
-
     var log = `${Date.now()}: ${req.url} request received.\n `;
+    if(req.url === '/favicon.ico') return res.end();
+
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
+    
     fs.appendFile('log.txt', log, (err) =>
     {
-        if(err) {
-            res.end(err);
+        switch(myUrl.pathname)
+        {
+            case '/':
+                res.end('Home');
+                break;
+            case '/about':
+                const username = myUrl.query.myname;
+                res.end(`Hi! ${username}`);
+                break;
+            case '/search':
+                const mysearch = myUrl.query.search_query;
+                res.end(`The search result is: ${mysearch}`);
+                break;
+            default:
+                res.end('This not a valid one');
+                break;
         }
-        else {
-            res.end("Hi there, you are in http server");
-        }
-    })
+    });
 });
 
 server.listen(8000, () => {
