04fb04c9bca416552adc17f539ec9a62f4be62dc
Nutileety <nutileety@gmail.com>
2024-12-16 11:26:19 +0530

    added code to access on the first_name of the users in list html

--- node_practice/REST APIs practice/index.js
+++ node_practice/REST APIs practice/index.js
@@ -3,9 +3,17 @@ const users = require('./MOCK_DATA.json');
 const app = express();
 const port = 8000;
 
-app.get('/users', (req, res) => {
-    res.send(users);
+//only as APIs
+app.get('/apis/users', (req, res) => {
+   return res.send(users);
 });
+
+app.get('/users', (req, res) =>{
+   const html = `<ul> ${users.map((user) => `<li> ${user.first_name} </li> `).join("")} </ul>`;
+   return res.send(html);
+});
+
+
 
 app.listen(port, () => {
     console.log(`The server is started on port: ${port}`);
