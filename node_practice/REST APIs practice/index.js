3e30571e5d126a05a63051ae5d533f02b643da83
Nutileety <nutileety@gmail.com>
2024-12-16 12:20:22 +0530

    added fetching user based on individual id's '/apis/users/:id'

--- node_practice/REST APIs practice/index.js
+++ node_practice/REST APIs practice/index.js
@@ -5,15 +5,31 @@ const port = 8000;
 
 //only as APIs
 app.get('/apis/users', (req, res) => {
-   return res.send(users);
+   return res.json(users);
 });
 
+// extracting only the first_name from the array object
 app.get('/users', (req, res) =>{
    const html = `<ul> ${users.map((user) => `<li> ${user.first_name} </li> `).join("")} </ul>`;
    return res.send(html);
 });
 
-
+// requesting for the particular id as parameter
+app.route('/apis/users/:id')
+.get((req, res) => {
+   const id = Number(req.params.id);
+   const user = users.find((user) => user.id === id); 
+   return res.json(user);
+})
+.put((req, res) => {
+   console.log("Pending...");
+})
+.patch((req,res) => {
+   console.log("Pending...");
+})
+.delete((req, res) => {
+   console.log("Pending...");
+});
 
 app.listen(port, () => {
     console.log(`The server is started on port: ${port}`);
