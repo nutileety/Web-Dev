113ce0b8bfd7476baeee46385d7735327b0218f4
Nutileety <nutileety@gmail.com>
2025-01-01 20:12:19 +0530

    practing post ex

--- node_practice/index.js
+++ node_practice/index.js
@@ -8,10 +8,12 @@ const users = [{
     }]
 }];
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     const johnKidneys = users[0].kidneys;
     const numberOfKidneys = johnKidneys.length;
-    const numberOfhealthyKidneys = 0;
+    let numberOfhealthyKidneys = 0;
     for(let i=0; i<numberOfKidneys; i++) {
         if(johnKidneys[i].healthy) {
             numberOfhealthyKidneys += 1;
@@ -21,6 +23,16 @@ app.get('/', (req, res) => {
     res.json({numberOfKidneys,
         numberOfhealthyKidneys,
         numberOfUnhealthyKidneys
+    });
+});
+
+app.post('/', (req, res) => {
+    const isHealthy = req.body.isHealthy;
+    users[0].kidneys.push({
+        healthy: isHealthy 
+    });
+    res.json({
+        msg: "Done"
     });
 });
 
