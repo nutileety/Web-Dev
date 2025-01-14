467c77b810d788015648a5f57f43da88ceeabf24
Nutileety <nutileety@gmail.com>
2025-01-14 19:57:57 +0530

    completed the put method which pending

--- node_practice/simple_todo/todo.js
+++ node_practice/simple_todo/todo.js
@@ -7,7 +7,9 @@ const todos = require('./todos_list.json');
 
 app.use(express.json());
 
-app.get('/todo', (req, res) => {
+//created a route for get the data and post the data to file by /todo  
+app.route('/todo')
+.get((req, res) => {
     fs.readFile('todos_list.json', 'utf-8', (err, data) => {
         if(err) {
             res.status(404).send("unable to read the file..!");
@@ -16,19 +18,9 @@ app.get('/todo', (req, res) => {
             const jsonData = JSON.parse(data);
             res.json(jsonData);
         }
-    })
-});
-
-app.get('/todo/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const todo = todos.find((todo) => todo.id === id); 
-    return res.json({todo});
-});
-app.put('/todo/:id', (req, res) => {
-   
-});
-
-app.post('/todo', (req, res) => {
+    });
+})
+.post((req, res) => {
     const body = req.body;
     todos.push({id: todos.length+1, ...body});
     fs.writeFile('todos_list.json', JSON.stringify(todos), (err, data) => {
@@ -39,8 +31,32 @@ app.post('/todo', (req, res) => {
             res.send({status: "Success", id: todos.length});
         }
     });
-    
 });
+
+//routing the /todo/:id for get and put to modify made by requested id
+app.route('/todo/:id')
+.get((req, res) => {
+    const id = Number(req.params.id);
+    const todo = todos.find((todo) => todo.id === id); 
+    return res.json({todo});
+})
+.put((req, res) => {
+   const id = Number(req.params.id);
+   const todoIndex = todos.findIndex((todo) => todo.id === id); 
+   todos[todoIndex] = {...todos[todoIndex], ...req.body};
+   if(todos[todoIndex] === -1) {
+    return res.status(404).send("The id not found");
+   }
+   fs.writeFile('todos_list.json', JSON.stringify(todos), (err) => {
+        if(err) {
+            return res.status(404).send("Unable to put data..!");
+        }
+        else {
+            return res.status(200).json({status: "modified successfully", id:id});
+        }
+   });
+});
+
 
 app.listen(port, () => {
     console.log(`The server is started at port ${port}`);
