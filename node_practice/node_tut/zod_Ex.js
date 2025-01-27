de3bd2b2370e2995e5121df788185ca0f4d4a7e2
Nutileety <nutileety@gmail.com>
2025-01-27 19:37:00 +0530

    playing with zod

--- node_practice/node_tut/zod_Ex.js
+++ node_practice/node_tut/zod_Ex.js
@@ -1,17 +1,27 @@
 const zod = require('zod');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
 
 function validate(obj) {
     const schema = zod.object({
         email: zod.string().email(),
         password: zod.string().min(8)
-    })
-
-const response = schema.safeParse(obj);
-console.log(response);
+    });
+    const response = schema.safeParse(obj);
+    return response;
 }
 
-validate({
-    email: "abc@gmail.com",
-    password: "1234556789"
+app.post('/login', (req, res) => {
+    const response = validate(req.body);
+    if(!response.success) {
+        res.json({msg: "The input is invalid"});
+    }
+    return res.json(response);
 });
 
+
+app.listen(3000, () => {
+    console.log("The server is started..!");
+});
