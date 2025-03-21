b61117ca33c4dcbc296fa8b5289454c6aa2c5453
Nutileety <nutileety@gmail.com>
2025-03-21 20:19:25 +0530

    Practicing the simple fullstack login page

---
+++ node_practice/simple login page/login.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const app = express();
+const SECRET = "123thetokenisencoding";
+
+app.use(express.json());
+
+const users = [];
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+})
+
+app.post('/signup', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    users.push({
+        username: username,
+        password: password
+    });
+
+    res.json({
+        msg: "Signed up successfully"
+    });
+});
+
+app.post('/signin', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    let foundUser = null;
+    
+    for(let i=0; i<users.length; i++){
+        if(users[i].username === username && users[i].password === password) {
+            foundUser = users[i];
+        }
+        else{
+            return res.send("The Invalid user credentials");
+        }
+    }
+
+    if(foundUser) {
+        const token = jwt.sign({username: foundUser.username}, SECRET);
+        return res.json({token});
+    }
+    else {
+        return res.send("Unable to generate token");
+    }  
+});
+
+function auth(req, res, next) {
+    const token = req.headers.token;
+    const decodeUser = jwt.verify(token, SECRET);
+
+
+    if(decodeUser.username){
+        req.username = decodeUser.username;
+        next()
+    }
+    else{
+        return res.send("Invalid token credentials");
+    }
+}
+    
+
+app.get('/me',auth, (req, res) => {
+    let foundUser = null
+    for(let i=0; i<users.length; i++){
+        if(users[i].username === req.username) {
+            foundUser = users[i]
+        }
+    }
+    return res.json({
+        username: foundUser.username,
+    });
+})
+
+app.listen(3000, () => {
+    console.log("The server is started...");
+})
+
