7da95bab8d259d23ad21d3efeb40452a8c2236b5
Nutileety <nutileety@gmail.com>
2025-05-29 20:20:30 +0530

    todo with added validation

--- node_practice/todo_with_db/index.js
+++ node_practice/todo_with_db/index.js
@@ -4,6 +4,9 @@ const {userModel, todoModel} = require('./db');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const JWT_SECRET = '12345';
+const { z } = require('zod');
+const { required } = require('zod/v4-mini');
+
 
 const app = express();
 mongoose.connect("mongodb://localhost:27017/todo_db");
@@ -11,20 +14,35 @@ mongoose.connect("mongodb://localhost:27017/todo_db");
 app.use(express.json())
 
 app.post('/signup', async function(req, res) {
-    const email = req.body.email;
-    const password = req.body.password;
-    const name = req.body.name;
-    
-    const hashedPassword = await bcrypt.hash(password, 5);
+    const requiredBody = z.object({
+        email: z.string().min(3).max(100).email(),
+        name: z.string().min(3).max(100),
+        password: z.string().min(8).max(20)
+    });
 
-    await userModel.create({
-        email: email,
-        password: hashedPassword,
-        name: name
-    });
-    res.json({ 
-        msg: "signed up successfully"
-    });
+    const parseBody = requiredBody.safeParse(req.body);
+
+    if(!parseBody.success) {
+        res.json({
+            msg: "incorrect format",
+            error: parseBody.error
+        })
+    }else{
+        const email = req.body.email;
+        const password = req.body.password;
+        const name = req.body.name;
+        
+        const hashedPassword = await bcrypt.hash(password, 5);
+
+        await userModel.create({
+            email: email,
+            password: hashedPassword,
+            name: name
+        });
+        res.json({ 
+            msg: "signed up successfully"
+        });
+    }
 });
 
 app.post('/signin', async function(req, res) {
