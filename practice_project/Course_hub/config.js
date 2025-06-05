fe486dae5b3eec0f7efe4c0c1df9b8545746fcef
Nutileety <nutileety@gmail.com>
2025-06-05 12:59:09 +0530

    dotenv module is imported for the environment variables

--- practice_project/Course_hub/config.js
+++ practice_project/Course_hub/config.js
@@ -1,5 +1,5 @@
-const  JWT_USER_SECRET = 'user12345';
-const  JWT_ADMIN_SECRET = 'admin12345';
+const  JWT_USER_SECRET = process.env.JWT_USER_SECRET;
+const  JWT_ADMIN_SECRET = process.env.JWT_ADMIN_SECRET;
 
 module.exports = ({
     JWT_ADMIN_SECRET,
--- practice_project/Course_hub/index.js
+++ practice_project/Course_hub/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const { userRoute } = require('./Routes/user');
@@ -11,7 +12,7 @@ app.use('/api/v1/admin', adminRoute);
 app.use('/api/v1/course', courseRoute);
 
 async function main() {
-    await mongoose.connect('mongodb://localhost:27017/course-app');
+    await mongoose.connect(process.env.mongoURL);
     app.listen(3000, () => {
         console.log("The server is started...");
     })
