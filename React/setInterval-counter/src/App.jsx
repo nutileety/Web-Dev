8c3405f4553542dbd44f2eec62cba8db69899bf1
Nutileety <nutileety@gmail.com>
2025-06-17 23:13:27 +0530

    practice example of counter on setInterval

---
+++ React/setInterval-counter/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect, useReducer } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1>Counter</h1>
+    <Counter></Counter>
+    </>
+  )
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(function() {
+    setInterval(function() {
+      setCount(count => count + 1)
+    }, 1000)
+  }, [])
+
+
+  return (
+    <>
+      <h1>{count}</h1>
+    </>
+  )
+}
+
+
+export default App
