d252d53e7bf8f159931e622a57d7ed5acad72602
Nutileety <nutileety@gmail.com>
2025-06-26 13:56:45 +0530

    practice style in react

--- React/ui-practice/src/App.jsx
+++ React/ui-practice/src/App.jsx
@@ -1,17 +1,22 @@
 
 function App() {
+  const imgAddress = "https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?cs=srgb&dl=pexels-sulimansallehi-1704488.jpg&fm=jpg"
+
   return (
-    <div style={{backgroundColor:"#dfe6e9", hieght:"100vh"}}>
+    <div style={{backgroundColor:"#dfe6e9", hieght:"100vh", display:"flex"}}>
+      <div>
+        <ProfileCard imgAddress={imgAddress}/>
+      </div>
       <div style={{display:"flex", justifyContent:"center"}}>
         <div>
           <div>
-            <PostComponent />
+            <PostComponent imgAddress={imgAddress} />
           </div>
           <div>
-            <PostComponent />
+            <PostComponent imgAddress={imgAddress} />
           </div>
           <div>
-            <PostComponent />
+            <PostComponent imgAddress={imgAddress} />
           </div>
         </div>
       </div>
@@ -20,7 +25,7 @@ function App() {
   
 }
 
-function PostComponent() {
+function PostComponent(props) {
   const style = {
     width:"250", 
     backgroundColor:"white", 
@@ -28,12 +33,11 @@ function PostComponent() {
     margin:20,
     padding:30,
   }
-  const imgAddress = "https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?cs=srgb&dl=pexels-sulimansallehi-1704488.jpg&fm=jpg"
-
+  
   return (
     <div style={style}>
       <div style={{display:"flex"}}>
-        <img src={imgAddress} style={{width: 40, height:40, borderRadius:40}}/>
+        <img src={props.imgAddress} style={{width: 40, height:40, borderRadius:40}}/>
         <div style={{fontSize: 14, marginLeft:10}}>
           <b>1000x devs</b>
           <div>24999 followers</div>
@@ -42,6 +46,33 @@ function PostComponent() {
       </div>
       <div style={{ fontSize: 14 }}>
         What to know how to win big? Check out how these folks won $6000 in bounties.
+      </div>
+    </div>
+  )
+}
+
+function ProfileCard(props) {
+  return (
+    <div style={{backgroundColor:"white", margin:20, borderRadius:10,height:250, justifyItems:"center", textAlign:"center"  }}>
+      <div style={{width:250, backgroundColor:"#666669",height:50, borderTopLeftRadius:10, borderTopRightRadius:10, marginTop:20}}>     
+        <div>
+          <img src={props.imgAddress} style={{ width:80, height:80, borderRadius:80 }}/>
+        </div>
+        <div>
+          <div style={{fontWeight: "bold", fontSize:20 }}>1000x dev</div>
+          <div style={{fontSize:20}}>Works with XXX</div>
+          <hr />
+          <div style={{fontSize:16, textAlign:"justify", padding:20}}>
+            <table>
+              <tr >
+                <th style={{ textAlign: "left" }}>Profile Viewers</th>
+                <td style={{ textAlign: "right" }}>13600</td>
+              </tr>
+                <th style={{ textAlign: "left" }}>Profile Impression</th>
+                <td style={{ textAlign: "right", paddingLeft:40}}>4500</td>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   )
