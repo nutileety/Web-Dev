b701d54b6991c09de38a47ca70a0695617f101f7
Nutileety <nutileety@gmail.com>
2025-07-15 23:31:22 +0530

    Practice file: noPage.jsx from 2025-07-15 23:31:22

---
+++ React/simple-webapp/pages/noPage.jsx
@@ -0,0 +1,7 @@
+export default function NoPage() {
+  return (
+      <div style={{ backgroundColor: "red", alignContent:"center", width: "150px", height: "50px"}}>
+        Page is not available!
+      </div>
+  )
+}
