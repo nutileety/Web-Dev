5a4b329c57b323d762dce05ae79f48c2edf00b05
Nutileety <nutileety@gmail.com>
2025-08-07 12:17:18 +0530

    Practice file: atomFamily.js from 2025-08-07 12:17:18

---
+++ React/recoils/src/store/atoms/atomFamily.js
@@ -0,0 +1,19 @@
+import TODOS from "../TODOS";
+import { atomFamily, selectorFamily } from "recoil";
+import axios from "axios";
+
+export const todoAtomFamily = atomFamily({
+    key: "TodoFamily",
+     default: selectorFamily({
+      key: "todoSelectorfamily",
+      get: (id) => async ({get})=> {
+          await new Promise(r => setTimeout(r, 5000)); 
+          const res = await axios.get(`https://json-placeholder.mock.beeceptor.com/todos/${id}`)
+          return res.data//.find(x => x.id === id)
+        }
+     })
+       //id => {
+    //   return TODOS.find(x => x.id == id)
+    //}
+})
+
