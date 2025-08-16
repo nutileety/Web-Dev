f0441d0b7f40a72ae63fa2fedc093b9184fd5647
Nutileety <nutileety@gmail.com>
2025-08-16 12:49:45 +0530

    pracitced sidebar transition in tailwind

---
+++ Tailwind-in-React/profile_page/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { SidebarClass } from './componenets/sidebarClass'
+
+function App() {
+  return (
+    <>
+      <div>
+        <SidebarClass />
+      </div>
+    </>
+  )
+}
+
+export default App
