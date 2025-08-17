b7bc6b16d433b4176b264472f406becf95e4f6d4
Nutileety <nutileety@gmail.com>
2025-08-17 20:01:29 +0530

    Practice file: App.jsx from 2025-08-17 20:01:29

---
+++ Tailwind-in-React/profile_page/src/App.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react'
+import './App.css'
+import { SidebarClass } from './componenets/sidebarClass'
+import { SidebarToggle } from './icons/sidebarToggle'
+
+function App() {
+  const [ sidebarOpen, setSidebarOpen ] = useState(true)
+
+  return (
+    <>
+      <div className='flex'>
+        {/* <SidebarClass /> */}
+        <SideBar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        <MainContent /> 
+      </div>
+    </>
+  )
+}
+
+function SideBar({sidebarOpen, setSidebarOpen}) {
+  if(sidebarOpen) {
+    return (
+      <div className='transistion-all duration-1000 sm:w-56 sm:relative  bg-amber-200 absolute w-28 h-screen'>
+        <div className='top-0 left-0 cursor-pointer transition-all duration-1000' onClick= { () => {
+          setSidebarOpen(!sidebarOpen)
+        }}>
+          <SidebarToggle />
+        </div> 
+      </div>
+    )
+  }
+  else{
+    return (
+      <div className='transtion-all duration-1000' onClick={ () => {
+          setSidebarOpen(!sidebarOpen)
+        }}>
+        <div className='fixed left-0 top-0 cursor-pointer sm:text-white' >
+          <SidebarToggle />
+        </div>
+      </div>
+    ) 
+  }
+}
+
+function MainContent() {
+  console.log("hi")
+
+  return ( 
+    <div className='w-full'>
+      <div className='sm:h-30 sm:block hidden sm:bg-slate-950 col-span-11 '>
+
+      </div>
+      <div className='grid grid-cols-10 gap-5 p-7' >
+        <div className='sm:h-72 sm:block hidden sm:bg-red-300 rounded-lg col-span-2 -translate-15 ml-12 pr-48'>
+          
+        </div>
+        <div className='h-72 bg-teal-200 rounded-lg sm:col-span-5 col-span-11'>
+          
+        </div>
+        <div className='h-72 bg-yellow-200 rounded-lg sm:col-span-3 col-span-11'>
+          
+        </div>
+      </div>
+    </div>
+    
+  )
+}
+
+export default App
