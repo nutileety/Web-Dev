f60279ea6f3bd1a63097877d10b377700d7a0234
Nutileety <nutileety@gmail.com>
2026-07-24 12:28:54 +0530

    Practice file: readonly.js from 2026-07-24 12:28:54

---
+++ TypeScript/sum-of-age/dist/readonly.js
@@ -0,0 +1,10 @@
+const userProps = {
+    link: 'gpt', //changable
+    apikey: 234 //readonly
+};
+//  or 
+const userProps2 = {
+    link: 'openai', //readonly
+    apikey: 124 //readonly
+};
+export {};
