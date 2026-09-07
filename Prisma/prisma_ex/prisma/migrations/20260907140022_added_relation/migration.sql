d40e448fd18adc1f87ec8bb9af92b83dcf658f1a
Nutileety <nutileety@gmail.com>
2026-09-07 19:30:22 +0530

    Practice file: migration.sql from 2026-09-07 19:30:22

---
+++ Prisma/prisma_ex/prisma/migrations/20260907140022_added_relation/migration.sql
@@ -0,0 +1,36 @@
+/*
+  Warnings:
+
+  - You are about to drop the `user` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "user";
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL DEFAULT 'temp_password',
+    "age" INTEGER,
+    "city" TEXT NOT NULL DEFAULT 'unknown',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "discription" TEXT NOT NULL,
+    "done" BOOLEAN NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- AddForeignKey
+ALTER TABLE "Todo" ADD CONSTRAINT "Todo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
